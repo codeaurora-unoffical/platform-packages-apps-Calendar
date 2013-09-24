617610ac34d023fe96d6babe369aab2b4b6c8ed8
kayiz <kaiyiz@codeaurora.org>
2013-09-24 13:46:48 +0800

    Calendar: Fix force close after tap the map button in the notification
    When tap the map button in the notification, it will try to start map
    app. If no map app is installed in the device, it will cause
    ActivityNotFoundException.
    
    Catch the exception and popup toast to notice user.
    
    CRs-Fixed: 535447
    
    Change-Id: I51376f87a218da8e546c36559b56d6482f32d58f

--- src/com/android/calendar/alerts/AlertReceiver.java
+++ src/com/android/calendar/alerts/AlertReceiver.java
@@ -19,6 +19,7 @@ package com.android.calendar.alerts;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.Context;
@@ -42,6 +43,7 @@ import android.text.style.URLSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.android.calendar.R;
 import com.android.calendar.Utils;
@@ -119,7 +121,12 @@ public class AlertReceiver extends BroadcastReceiver {
                 Intent geoIntent = createMapActivityIntent(context, urlSpans);
                 if (geoIntent != null) {
                     // Location was successfully found, so dismiss the shade and start maps.
-                    context.startActivity(geoIntent);
+                    try {
+                        context.startActivity(geoIntent);
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(context, R.string.toast_no_map_app, Toast.LENGTH_SHORT)
+                                .show();
+                    }
                     closeNotificationShade(context);
                 } else {
                     // No location was found, so update all notifications.
